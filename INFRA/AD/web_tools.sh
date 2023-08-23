06f3d6d41cf82c8f7a9b39c89e9e8bbc4de602e4
OriolOriolOriol <claudio.rimensi.lavoro@gmail.com>
2023-08-23 09:19:05 -0400

    upload AD

---
+++ INFRA/AD/web_tools.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Elenco degli URL dei repository da scaricare
+repo_urls=(
+    "https://github.com/dirkjanm/krbrelayx.git",
+    "https://github.com/fortra/impacket.git",
+    "https://github.com/ly4k/Certipy.git"
+)
+
+# Cartella di destinazione per il download dei repository
+destination_folder="cartella_dei_repository"
+
+# Creazione della cartella di destinazione se non esiste
+mkdir -p "$destination_folder"
+
+# Ciclo attraverso gli URL dei repository e li scarica
+for url in "${repo_urls[@]}"; do
+    repo_name=$(basename "$url" .git)
+    repo_folder="$destination_folder/$repo_name"
+    
+    # Verifica se il repository è già stato clonato
+    if [ -d "$repo_folder" ]; then
+        echo "Il repository $repo_name esiste già. Aggiornamento..."
+        cd "$repo_folder"
+        git pull
+        cd ..
+    else
+        echo "Clonazione del repository $repo_name..."
+        git clone "$url" "$repo_folder"
+    fi
+done
+
+echo "Download completato!"
